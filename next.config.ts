3bd21b848b6f09312af56a2e0a1377ea08f3d3d0
vocterr <kozanecki.bartosz@interia.pl>
2025-01-18 16:13:28 +0100

    xd

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,28 @@ const nextConfig: NextConfig = {
         protocol: "http"
       }
     ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://luxry-backend.onrender.com/api/:path*"
+      }
+    ]
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true"},
+          { key: "Access-Control-Allow-Origin", value: "https://luxry.vercel.app"},
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,POST,PUT,DELETE"},
+          { key: "Access-Control-Allow-Headers", value: "Content-Type,Authorization"}
+        ]
+      }
+    ]
   }
 };
 
