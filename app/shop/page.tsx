c48994af15cad5c7c1268899068f88afbeac0908
vocterr <kozanecki.bartosz@interia.pl>
2025-01-18 16:40:37 +0100

    xd

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -30,8 +30,8 @@ export default async function ShopPage() {
                 <ShopPageOnClient userRole={userRole.role} carouselProducts={carouselProducts} initialProducts={initialProducts} />
             ) : (
                 <div className="fixed w-[350px] sm:w-auto text-white flex flex-col top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                    <h1 className="pb-6 md:text-xl 2xl:text-2xl border-b self-center border-gray-500">You don't have any items in your wishlist.</h1>
-                    <Image src="/animated/nowishlist.svg" alt="nowishlist" height={650} width={650} className="mt-6 h-60 md:h-72 xl:h-80 2xl:h-96" />
+                    <h1 className="pb-6 md:text-xl 2xl:text-2xl border-b self-center border-gray-500">There is an issue with our server, please try again later.</h1>
+                    <Image src="/animated/nousers.svg" alt="noproducts" height={650} width={650} className="mt-6 h-60 md:h-72 xl:h-80 2xl:h-96" />
                     <Link href="/shop" className="bg-gradient-to-r from-amber-600 via-pink-600 to-purple-600 rounded-lg shadow-md shadow-gray-800 py-2 text-center w-[70%] xl:w-[55%] font-bold text-lg mt-6 self-center">
                         Find Some Items
                     </Link>
